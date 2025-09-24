5010ad04b2a13a739c37bfe41dfd098d2474888f
regiealcovergenita0 <regiealcovergenita0@gmail.com>
2025-09-24 23:56:52 +0800

    This is My Main Branch

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import CounterApp from './CounterApp';
-import ColorChangerApp from './ColorChangerApp';
 
 export default function App() {
   return (
